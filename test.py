d8007a7fa8466364ccd6cee1911383fa774833be
Gabriel Tavares Gargur <ggargur@id.uff.br>
2025-05-13 09:54:36 -0300

    added serialization to model

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
+import os
+import pickle
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 import random
+
+
+caminho_arquivo = 'model.pkm'
 
 # Carrega o CSV
 df = pd.read_csv('pkm.csv')
@@ -50,8 +55,21 @@ def gerar_amostras(df, n_amostras=10000):
 X, y = gerar_amostras(df)
 
 # Modelo de classificação
-clf = RandomForestClassifier(n_estimators=100, random_state=42)
-clf.fit(X, y)
+
+clf = None
+if os.path.exists(caminho_arquivo):
+    # Arquivo existe: carrega o modelo
+    with open(caminho_arquivo, 'rb') as f:
+        clf = pickle.load(f)
+    print("Modelo carregado com sucesso!")
+else:
+    # Arquivo não existe: treina e salva um novo
+    clf = RandomForestClassifier(n_estimators=100, random_state=42)
+    clf.fit(X, y)
+    
+    with open(caminho_arquivo, 'wb') as f:
+        pickle.dump(clf, f)
+    print("Modelo treinado e salvo!")
 
 # Função para sugerir próximo pokémon
 def sugerir_pokemon(time, modelo, codificador):
@@ -68,13 +86,17 @@ sugestao = sugerir_pokemon(time_exemplo, clf, le)
 print(f"Sugestão de Pokémon para o time: {sugestao}")
 
 
-# SELECT 
-#   mon.name AS pokemon1,
-#   mon.usage AS uso_pokemon1, 
-#   mon.viability_ceiling AS viability_ceiling_pokemon1,
-#   mon2.name AS pokemon2, 
-#   mon2.usage AS uso_pokemon2, 
-#   mon2.viability_ceiling AS viability_ceiling_pokemon2
-# FROM mon
-# JOIN team t ON t.mon = mon.name
-# JOIN mon mon2 ON t.mate = mon2.name
+#.mode csv
+#.headers on
+#.output pkm.csv
+#SELECT 
+#  mon.name AS pokemon1,
+#  mon.usage AS uso_pokemon1, 
+#  mon.viability_ceiling AS viability_ceiling_pokemon1,
+#  mon2.name AS pokemon2, 
+#  mon2.usage AS uso_pokemon2, 
+#  mon2.viability_ceiling AS viability_ceiling_pokemon2
+#FROM mon
+#JOIN team t ON t.mon = mon.name
+#JOIN mon mon2 ON t.mate = mon2.name;
+#.output stdout
